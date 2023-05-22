719b64749f7e79b86ad744e328e62182c74b93ae
luispcunha <luispplmcunha@gmail.com>
2023-05-22 16:11:49 +0100

    Use SIGUSR1 to signal spot instance preemption

--- common/events/inductiva_api/events/tasks.py
+++ common/events/inductiva_api/events/tasks.py
@@ -13,24 +13,33 @@ class TaskCreated(TaskEvent):
 
 
 class TaskInputUploaded(TaskEvent):
-    pass
+    status: str = "submitted"
 
 
 class TaskStarted(TaskEvent):
+    status: str = "started"
     executer: str
 
 
 class TaskKillRequested(TaskEvent):
-    pass
+    status: str = "pending-kill"
 
 
 class TaskKilled(TaskEvent):
-    pass
+    status: str = "killed"
 
 
 class TaskCompleted(TaskEvent):
     pass
 
 
-class TaskResourcesPreempted(TaskEvent):
-    pass
+class SpotInstancePreempted(TaskEvent):
+    status: str = "spot-instance-preempted"
+
+
+class ExecuterTrackerTerminated(TaskEvent):
+    status: str = "executer-tracker-terminated"
+
+
+class ExecuterTrackerFailed(TaskEvent):
+    status: str = "executer-tracker-failed"
