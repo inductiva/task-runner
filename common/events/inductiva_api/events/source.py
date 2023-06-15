5722594161bd7d573bbf4b5140bd7e62ab224f8b
luispcunha <luispplmcunha@gmail.com>
2023-06-15 16:31:45 +0100

    Add support to monitor events with consumer groups

--- common/events/inductiva_api/events/source.py
+++ common/events/inductiva_api/events/source.py
@@ -1,5 +1,5 @@
 """Module with functionality for listening to events."""
-from typing import Iterator, Tuple
+from typing import Iterator, Optional, Tuple
 
 from absl import logging
 from inductiva_api.events import parse
@@ -10,8 +10,12 @@ from redis import Redis
 class EventSource:
     """Class for listening to events from Redis streams."""
 
-    def __init__(self, stream: str, redis_hostname: str,
-                 redis_port: int) -> None:
+    def __init__(self,
+                 stream: str,
+                 redis_hostname: str,
+                 redis_port: int,
+                 consumer_group: Optional[str] = None,
+                 consumer_name: Optional[str] = None) -> None:
         self._conn = Redis(
             redis_hostname,
             redis_port,
@@ -19,15 +23,40 @@ class EventSource:
             decode_responses=True,
         )
         self._stream = stream
+        self._consumer_group = consumer_group
+        self._consumer_name = consumer_name
+        self._use_consumer_group = not (consumer_group is None or
+                                        consumer_name is None)
+        logging.info("Using consumer group: %s", self._use_consumer_group)
+
+        if consumer_group is None and consumer_name is not None:
+            raise ValueError("A `consumer_group` must be provided if \
+                      `consumer_name` is provided.")
+
+        if consumer_name is None and consumer_group is not None:
+            raise ValueError("A `consumer_name` must be provided if \
+                    `consumer_group` is provided.")
+
+    def _read_stream(self, last_id: str):
+        if self._use_consumer_group:
+            return self._conn.xreadgroup(
+                self._consumer_group,
+                self._consumer_name,
+                {self._stream: last_id},
+                count=1,
+            )
+        else:
+            return self._conn.xread(
+                {self._stream: last_id},
+                count=1,
+            )
 
     def monitor(self, start_id="0-0") -> Iterator[Tuple[Event, str]]:
         last_id = start_id
         while True:
             try:
-                resp = self._conn.xread(
-                    {self._stream: last_id},
-                    count=1,
-                )
+                resp = self._read_stream(last_id)
+
                 if resp:
                     _, messages = resp[0]
                     last_id, data = messages[0]
@@ -35,6 +64,13 @@ class EventSource:
                     event = parse.from_dict(data)
 
                     yield event, last_id
+
+                    if self._use_consumer_group:
+                        self._conn.xack(
+                            self._stream,
+                            self._consumer_group,
+                            last_id,
+                        )
 
             except ConnectionError:
                 logging.exception("Error in Redis connection")
