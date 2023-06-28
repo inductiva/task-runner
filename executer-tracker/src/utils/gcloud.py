2f0413ed4ce74f484ea99ee8aec2e34cc382612c
luispcunha <luispplmcunha@gmail.com>
2023-06-28 13:27:24 +0100

    Rewrite get_vm_metadata

--- executer-tracker/src/utils/gcloud.py
+++ executer-tracker/src/utils/gcloud.py
@@ -31,20 +31,7 @@ def is_running_on_gcloud_vm() -> bool:
         r.headers.get(METADATA_FLAVOR_HEADER) == "Google"
 
 
-def get_vm_metadata(key: Optional[str] = None) -> Optional[Union[Dict, str]]:
-    """Get the metadata for the VM from the internal metadata server.
-
-    Args:
-        key: if provided, get the value for the given key. If not provided, get
-            the entire metadata as JSON.
-    """
-
-    get_recursive = True
-    url_suffix = "?recursive=true"
-    if key:
-        url_suffix = key
-        get_recursive = False
-
+def _get_vm_metadata(url_suffix: str):
     url = f"{METADATA_URL}{url_suffix}"
 
     try:
@@ -59,7 +46,33 @@ def get_vm_metadata(key: Optional[str] = None) -> Optional[Union[Dict, str]]:
     if r.status_code != 200:
         return None
 
-    return r.json() if get_recursive else r.text
+    return r
+
+
+def get_full_vm_metadata() -> Optional[Dict]:
+    """Get all metadata for the VM from the internal metadata server.
+
+    Returns all metadata as a dict.
+    """
+
+    url_suffix = "?recursive=true"
+    r = _get_vm_metadata(url_suffix)
+    if r is None:
+        return None
+
+    return r.json()
+
+
+def get_vm_metadata(key: str) -> Optional[str]:
+    """Get all metadata for the VM from the internal metadata server.
+
+    Returns all metadata as a dict.
+    """
+    r = _get_vm_metadata(key)
+    if r is None:
+        return None
+
+    return r.text
 
 
 @dataclass
@@ -73,10 +86,8 @@ class GCloudVMInfo:
 
 def get_vm_info() -> Optional[GCloudVMInfo]:
     """Get the VM information from the internal metadata server."""
-    metadata = get_vm_metadata()
+    metadata = get_full_vm_metadata()
     if not metadata:
-        return None
-    if not isinstance(metadata, dict):
         return None
 
     # Remove the attributes field, as it contains ssh keys
@@ -100,7 +111,7 @@ def is_vm_preempted() -> bool:
     https://cloud.google.com/compute/docs/instances/create-use-preemptible#determine_if_a_vm_was_preempted # pylint: disable=line-too-long
     """
     preempted = get_vm_metadata(key="preempted")
-    if not isinstance(preempted, str):
+    if preempted is None:
         return False
 
     return bool_string_to_bool(preempted)
