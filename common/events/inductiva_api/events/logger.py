a22a1a541127b23d68f9836016ce514eb8aee3b1
luispcunha <luispplmcunha@gmail.com>
2023-07-05 09:18:07 +0100

    Refactor usage of new Redis related functions

--- common/events/inductiva_api/events/logger.py
+++ common/events/inductiva_api/events/logger.py
@@ -1,18 +1,28 @@
 """Logging events to Redis."""
-from . import parse
 from redis.asyncio import Redis as AsyncRedis
 from redis import Redis
-from .schemas import Event
+
+from inductiva_api.events import parse
+from inductiva_api.events.schemas import Event
 
 
 class RedisStreamEventLogger:
     """Class that handles logging events to Redis."""
 
-    def __init__(self, stream_key: str):
+    def __init__(self, redis: AsyncRedis, stream_key: str):
+        self.redis = redis
         self.stream_key = stream_key
 
-    async def log(self, r: AsyncRedis, event: Event):
-        return await r.xadd(self.stream_key, parse.to_dict(event))
+    async def log(self, event: Event):
+        return await self.redis.xadd(self.stream_key, parse.to_dict(event))
 
-    def log_sync(self, r: Redis, event: Event):
-        return r.xadd(self.stream_key, parse.to_dict(event))
+
+class RedisStreamEventLoggerSync:
+    """Class that handles logging events to Redis."""
+
+    def __init__(self, redis: Redis, stream_key: str):
+        self.redis = redis
+        self.stream_key = stream_key
+
+    def log(self, event: Event):
+        return self.redis.xadd(self.stream_key, parse.to_dict(event))
