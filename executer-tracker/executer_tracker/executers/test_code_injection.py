e10be1647aa0f89577f0181812b31c1699826728
luispcunha <lpcunha@inductiva.ai>
2023-12-13 16:37:52 +0000

    Fix executer command tests

--- executer-tracker/executer_tracker/executers/test_code_injection.py
+++ executer-tracker/executer_tracker/executers/test_code_injection.py
@@ -5,24 +5,16 @@ import pytest
 from executer_tracker import executers
 
 
-class TestCommand(executers.Command):
-    """Dummy command class."""
-
-    def check_security(self, cmd, prompts):
-        executers.Command.check_security(cmd, prompts)
-        pass
-
-
 def test_empty_command():
     """Test empty command."""
     with pytest.raises(ValueError):
-        TestCommand("")
+        executers.Command("")
 
 
 def test_long_command():
     """Test long command."""
     with pytest.raises(ValueError):
-        TestCommand("a" * 257)
+        executers.Command("a" * 257)
 
 
 @pytest.mark.parametrize("cmd", [
@@ -33,7 +25,7 @@ def test_long_command():
 def test_command_concatenation_operator_characters(cmd):
     """Test command concatenation operator characters."""
     with pytest.raises(ValueError):
-        TestCommand(cmd)
+        executers.Command(cmd)
 
 
 @pytest.mark.parametrize("prompts", [
@@ -44,18 +36,4 @@ def test_command_concatenation_operator_characters(cmd):
 def test_prompt_invalid_characters(prompts):
     """Test invalid characters in prompts."""
     with pytest.raises(ValueError):
-        TestCommand("gmx protein.gro", prompts)
-
-
-@pytest.mark.parametrize("cmd, prompts", [
-    ("gmx pdb2gmx -f protein.pdb", ["amber94"]),
-    ("gmx pdb2gmx -f protein.pdb", ["amber94", "system"]),
-    ("gmx pdb2gmx -f protein.pdb -o protein_output.gro", ["amber94", "system"]),
-    ("gmx pdb2gmx -f", ["amber94", "system"])
-])
-def test_valid_init(cmd, prompts):
-    """Test valid initialization."""
-    command = TestCommand(cmd, prompts)
-
-    assert command.cmd == cmd
-    assert command.prompts == prompts
+        executers.Command("gmx protein.gro", prompts)
