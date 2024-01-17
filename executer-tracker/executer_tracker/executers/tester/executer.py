0e6086288d39a07ed40db1ec97ce4b65f9822550
IvanPombo <ivanpombo@inductiva.ai>
2024-01-17 16:46:46 +0000

    Add TestExecuter to backend

--- executer-tracker/executer_tracker/executers/tester/executer.py
+++ executer-tracker/executer_tracker/executers/tester/executer.py
@@ -1,18 +1,30 @@
 import os
 import shutil
-import psutil
+
+import time
 
 from executer_tracker import executers
+
+
 
 class TestExecuter(executers.BaseExecuter):
 
     def execute(self):
-        n_cores = psutil.cpu_count(logical=False)
         input_file = self.args.input_filename
         input_dir = os.path.join(self.working_dir, self.args.sim_dir)
         commands = self.args.commands
+        sleep_time = self.args.sleep_time
 
         shutil.copytree(input_dir, self.artifacts_dir, dirs_exist_ok=True)
+        os.chdir(self.artifacts_dir)
+        
+        with open("test_arguments.txt", "w", encoding="utf-8") as f:
+            f.write(f"Input file: {input_file}")
+            f.write(f"Input directory: {self.args.sim_dir}")
+            f.write(f"Commands: {commands}")
+            f.write(f"Sleep time: {sleep_time}")
+
+        time.sleep(sleep_time)
 
         for command in commands:
             command = executers.Command(command["cmd"], command["prompts"])
