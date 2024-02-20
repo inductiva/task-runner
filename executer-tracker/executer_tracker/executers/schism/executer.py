da7acfdd9fd733ec67dfd7c4da29b20902b823f1
augusto peres <augustoluisperes@gmail.com>
2024-02-20 09:46:55 +0000

    final bug fixes

--- executer-tracker/executer_tracker/executers/schism/executer.py
+++ executer-tracker/executer_tracker/executers/schism/executer.py
@@ -13,10 +13,10 @@ class SCHISMExecuter(executers.BaseExecuter):
         shutil.copytree(input_dir, self.artifacts_dir, dirs_exist_ok=True)
 
         n_vcpus = self.args.n_vcpus
-        num_scribes = self.arg.num_scribes
+        num_scribes = self.args.num_scribes
 
         # The simulator expects a directory "outputs" to store the outputs.
-        os.mkdir(os.path.join(artifcats_dir, "outputs"))
+        os.mkdir(os.path.join(self.artifacts_dir, "outputs"))
 
         cmd = executers.Command(
             f"mpirun --np {n_vcpus} /schism/build/bin/pschism {num_scribes}")
