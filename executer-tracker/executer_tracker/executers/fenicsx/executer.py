3a161b500e61317d0194ffc06d280987a22be1a3
João Ribeiro <jp.ar@hotmail.com>
2024-03-05 16:02:12 +0000

    yapf

--- executer-tracker/executer_tracker/executers/fenicsx/executer.py
+++ executer-tracker/executer_tracker/executers/fenicsx/executer.py
@@ -42,8 +42,8 @@ class LinearElasticityFEniCSxExecuter(executers.BaseExecuter):
         material_path = os.path.join(sim_dir_path, self.args.material_filename)
         element_family = self.args.mesh_element_family
         element_order = self.args.mesh_element_order
-        quadrature_rule=self.args.mesh_quadrature_rule
-        quadrature_degree=self.args.mesh_quadrature_degree
+        quadrature_rule = self.args.mesh_quadrature_rule
+        quadrature_degree = self.args.mesh_quadrature_degree
         results_dir = self.artifacts_dir
 
         cmd = executers.Command("python /scripts/elastic_case/elastic_case.py "
