6ee3544e6206f258f3ed0c8989d05930b9742e38
João Ribeiro <jp.ar@hotmail.com>
2024-03-08 11:13:42 +0000

    mesh_as_input

--- executer-tracker/executer_tracker/executers/fenicsx/executer.py
+++ executer-tracker/executer_tracker/executers/fenicsx/executer.py
@@ -10,7 +10,6 @@ from executer_tracker.executers.fenicsx.geometry import geometry_utils
 MESH_FILENAME = "mesh.msh"
 MESH_INFO_FILENAME = "mesh_info.json"
 
-
 class LinearElasticityFEniCSxExecuter(executers.BaseExecuter):
     """Concrete implementation of an Executer to run FEniCSx."""
 
@@ -19,21 +18,39 @@ class LinearElasticityFEniCSxExecuter(executers.BaseExecuter):
 
         # Geometry file
         geometry_path = os.path.join(sim_dir_path, self.args.geometry_filename)
-        geometry = geometry_utils.GeometricCase.from_json(geometry_path)
         shutil.copy(geometry_path, self.artifacts_dir)
 
-        # Mesh file
-        mesh_path = os.path.join(sim_dir_path, MESH_FILENAME)
-        mesh = mesh_utils.GmshMesh(
-            geometry=geometry,
-            global_refinement_factor=self.args.global_refinement_meshing_factor,
-            local_refinement_factor=self.args.local_refinement_meshing_factor,
-            smoothing_parameter=self.args.smoothing_meshing_parameter)
-        mesh.write_to_msh(mesh_path)
+        # Check if a custom mesh is provided
+        if hasattr(self.args, "mesh_filename") and self.args.mesh_filename:
+            mesh_path = os.path.join(sim_dir_path, self.args.mesh_filename)
+            os.rename(mesh_path, os.path.join(sim_dir_path, MESH_FILENAME))
 
-        # Mesh information file
-        mesh_info_path = os.path.join(self.artifacts_dir, MESH_INFO_FILENAME)
-        mesh.write_mesh_info_to_json(mesh_info_path)
+            # If mesh info filename is provided
+            if hasattr(self.args,
+                       "mesh_info_filename") and self.args.mesh_info_filename:
+                mesh_info_path = os.path.join(sim_dir_path,
+                                              self.args.mesh_info_filename)
+                shutil.copy(
+                    mesh_info_path,
+                    os.path.join(self.artifacts_dir, MESH_INFO_FILENAME))
+
+        else:
+            # Generate mesh file
+            mesh_path = os.path.join(sim_dir_path, MESH_FILENAME)
+            geometry = geometry_utils.GeometricCase.from_json(geometry_path)
+            mesh = mesh_utils.GmshMesh(
+                geometry=geometry,
+                global_refinement_factor=self.args.
+                global_refinement_meshing_factor,
+                local_refinement_factor=self.args.
+                local_refinement_meshing_factor,
+                smoothing_parameter=self.args.smoothing_meshing_parameter)
+            mesh.write_to_msh(mesh_path)
+
+            # Mesh information file
+            mesh_info_path = os.path.join(self.artifacts_dir,
+                                          MESH_INFO_FILENAME)
+            mesh.write_mesh_info_to_json(mesh_info_path)
 
     def execute(self):
         sim_dir_path = os.path.join(self.working_dir, self.args.sim_dir)
