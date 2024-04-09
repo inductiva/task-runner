95de8c4521d9bee57c13e9ec0fa25d6bc7abb297
Franreno <franreno4@gmail.com>
2024-04-09 14:56:07 +0100

    Remove F403 of ignore list

--- common/events/inductiva_api/events/__init__.py
+++ common/events/inductiva_api/events/__init__.py
@@ -1,5 +1,5 @@
 # noqa: D104
-from .schemas import *
+from .schemas import *  # noqa: F403
 from .logger import RedisStreamEventLogger, RedisStreamEventLoggerSync
 from .parse import to_dict, from_dict
 from .source import RedisStreamEventSource
