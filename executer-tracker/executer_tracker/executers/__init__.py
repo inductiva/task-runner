ea2c339316d5b2a4f9f171c84d765f4a864375e6
Franreno <franreno4@gmail.com>
2024-04-10 14:30:34 +0100

    Fixes circular import on executers

--- executer-tracker/executer_tracker/executers/__init__.py
+++ executer-tracker/executer_tracker/executers/__init__.py
@@ -4,6 +4,11 @@ This submodule includes the Executer classes that are used to run
 API methods, along with utility functions and classes that are used
 by the Executer classes.
 """
+from .base_executer import BaseExecuter  # noqa: I001
+from .command import Command  # noqa: I001
+from .mpi_base_executer import MPIExecuter  # noqa: I001
+from .mpi_configuration import MPIConfiguration  # noqa: I001
+from .subprocess_tracker import SubprocessTracker  # noqa: I001
 from . import (
     dualsphysics,
     dummy,
@@ -22,8 +27,3 @@ from . import (
     utils,
     xbeach,
 )
-from .base_executer import BaseExecuter
-from .command import Command
-from .mpi_base_executer import MPIExecuter
-from .mpi_configuration import MPIConfiguration
-from .subprocess_tracker import SubprocessTracker
