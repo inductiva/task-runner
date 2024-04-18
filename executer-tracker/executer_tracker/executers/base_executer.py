c9b915ee4f4a0703e91be09abb20a93a29d1418c
luispcunha <luispplmcunha@gmail.com>
2024-04-18 12:12:43 +0100

    Fix mistakenly added \n

--- executer-tracker/executer_tracker/executers/base_executer.py
+++ executer-tracker/executer_tracker/executers/base_executer.py
@@ -212,7 +212,7 @@ class BaseExecuter(ABC):
         with open(self.stdout_logs_path, "a", encoding="UTF-8") as stdout, \
             open(self.stderr_logs_path, "a", encoding="UTF-8") as stderr, \
                 open(stdin_path, "r", encoding="UTF-8") as stdin:
-            log_message = f"# COMMAND: {cmd.args}\n"
+            log_message = f"# COMMAND: {cmd.args}"
             self.loki_logger.log_text(log_message, io_type=loki.IOTypes.COMMAND)
             log_message += "\n"
             stdout.write(log_message)
