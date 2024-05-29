f9797a86f980c6e202d77a8e8a1dd58336688c09
Bruno Grego <bgrego@inductiva.ai>
2024-05-29 12:28:55 +0100

    Improve task metrics first draft

--- executer-tracker/executer_tracker/utils/files.py
+++ executer-tracker/executer_tracker/utils/files.py
@@ -2,36 +2,66 @@
 import os
 import shutil
 import tempfile
+import time
 import zipfile
 
 import fsspec
 from absl import logging
 
 
-def make_zip_archive(zip_path, source_dir):
-    # make_archive expects the path without extension
-    zip_path_no_ext = os.path.splitext(zip_path)[0]
+def calculate_duration(start_time: float) -> float:
+    return time.time() - start_time
 
+
+def make_zip_archive(zip_path: str, source_dir: str) -> float:
+    # make_archive expects the path without extension
+    zip_start = time.time()
+
+    zip_path_no_ext = os.path.splitext(zip_path)[0]
     zip_path = shutil.make_archive(zip_path_no_ext, "zip", source_dir)
+
+    duration = calculate_duration(zip_start)
 
     logging.info("Created zip archive: %s", zip_path)
 
-    return zip_path
+    return duration
 
 
-def extract_zip_archive(zip_path, dest_dir):
+# TODO: review if type annotations are correct
+def extract_zip_archive(zip_path: str, dest_dir: str) -> float:
     """Extract ZIP archive.
 
     Args:
         zip_path: Path to the ZIP file.
         dest_dir: Directory where to write the uncompressed files.
     """
+    unzip_start = time.time()
+
     with zipfile.ZipFile(zip_path, "r") as zip_fp:
         zip_fp.extractall(dest_dir)
 
+    return calculate_duration(unzip_start)
 
-def download_and_extract_zip_archive(filesystem: fsspec.AbstractFileSystem,
-                                     remote_path: str, dest_dir: str):
+
+def download_file(
+    filesystem: fsspec.AbstractFileSystem,
+    remote_path: str,
+    local_path: str,
+):
+    download_start = time.time()
+
+    with filesystem.open(remote_path, "rb") as f:
+        with open(local_path, "wb") as local_file:
+            shutil.copyfileobj(f, local_file)
+
+    return calculate_duration(download_start)
+
+
+def download_and_extract_zip_archive(
+    filesystem: fsspec.AbstractFileSystem,
+    remote_path: str,
+    dest_dir: str,
+):
     """Download and extract ZIP archive from fsspec filesystem.
 
     Args:
@@ -43,22 +73,35 @@ def download_and_extract_zip_archive(filesystem: fsspec.AbstractFileSystem,
     with tempfile.TemporaryDirectory() as tmp_dir:
         tmp_zip_path = os.path.join(tmp_dir, "file.zip")
 
-        with filesystem.open(remote_path, "rb") as f:
-            with open(tmp_zip_path, "wb") as local_file:
-                shutil.copyfileobj(f, local_file)
+        download_duration = download_file(
+            filesystem=filesystem,
+            remote_path=remote_path,
+            local_path=tmp_zip_path,
+        )
+        # TODO: push duration to API
+        print(download_duration)
 
         logging.info("Downloaded zip to: %s", tmp_zip_path)
 
-        extract_zip_archive(
+        unzip_duration = extract_zip_archive(
             zip_path=tmp_zip_path,
             dest_dir=dest_dir,
         )
+        # TODO: push duration to API
+        print(unzip_duration)
 
         logging.info("Extracted zip to: %s", dest_dir)
 
 
-def upload_file(filesystem: fsspec.AbstractFileSystem, file_path,
-                file_path_remote):
-    with open(file_path, "rb") as f_src:
-        with filesystem.open(file_path_remote, "wb") as f_dest:
+def upload_file(
+    filesystem: fsspec.AbstractFileSystem,
+    local_path: str,
+    remote_path: str,
+):
+    upload_start = time.time()
+
+    with open(local_path, "rb") as f_src:
+        with filesystem.open(remote_path, "wb") as f_dest:
             shutil.copyfileobj(f_src, f_dest)
+
+    return calculate_duration(upload_start)
