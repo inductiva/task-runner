8bdbebbaabf54b51b0783f253ee4eaa0410703cf
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-07-15 12:03:47 +0100

    Change nwchem to MPIExecuter

--- executer-tracker/executer_tracker/executers/nwchem/executer.py
+++ executer-tracker/executer_tracker/executers/nwchem/executer.py
@@ -1,27 +1,23 @@
 """Generic Fire simulation with FDS."""
-import os
-import shutil
-
 from executer_tracker import executers
+from executer_tracker.executers import mpi_configuration
+from executer_tracker.utils import loki
 
 
-class NWChemExecuter(executers.BaseExecuter):
+class NWChemExecuter(executers.MPIExecuter):
     """Concrete implementation of an Executer to run NWChem."""
 
-    def execute(self):
-        sim_dir = os.path.join(self.working_dir, self.args.sim_dir)
-        input_filename = self.args.input_filename
-
-        if self.args.n_vcpus:
-            self.mpi_config.extra_args.extend(["-np", f"{self.args.n_vcpus}"])
-
-        if self.args.use_hwthread:
-            self.mpi_config.extra_args.extend(["--use-hwthread-cpus"])
-
-        # Copy the input files to the artifacts directory
-        shutil.copytree(sim_dir, self.artifacts_dir, dirs_exist_ok=True)
-
-        nwchem_bin = "nwchem"
-
-        cmd = executers.Command(f"{nwchem_bin} {input_filename}", is_mpi=True)
-        self.run_subprocess(cmd, working_dir=self.artifacts_dir)
+    def __init__(
+        self,
+        working_dir,
+        container_image,
+        mpi_config: mpi_configuration.MPIConfiguration,
+        loki_logger: loki.LokiLogger,
+    ):
+        super().__init__(working_dir=working_dir,
+                         container_image=container_image,
+                         loki_logger=loki_logger,
+                         mpi_config=mpi_config,
+                         sim_binary="nwchem",
+                         file_type="nw",
+                         sim_specific_input_filename="input.nw")
