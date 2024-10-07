228ecbb2281f5f4d035b3bd0761900a119781807
Rodrigo Tuna <rodrigotuna.andrade@gmail.com>
2024-10-07 17:12:36 +0100

    fix: fix quantumespresso import error

--- task-runner/task_runner/executers/__init__.py
+++ task-runner/task_runner/executers/__init__.py
@@ -11,6 +11,7 @@ from .mpi_configuration import MPIClusterConfiguration  # noqa: I001
 from .subprocess_tracker import SubprocessTracker  # noqa: I001
 from . import (
     arbitrary_commands_executer,
+    quantumespresso,
     dualsphysics,
     splishplash,
     openfast,
