27e938db67151c59d99ae98ad726f8d1c19b0aa0
Rodrigo Tuna <rodrigotuna.andrade@gmail.com>
2024-10-31 16:01:20 +0000

    feat: add one connection per operation and add recursive ls

--- file-tracker/file_tracker/file_tracker.py
+++ file-tracker/file_tracker/file_tracker.py
@@ -21,6 +21,8 @@ INTERNAL_ICE_SERVERS = [{
     "urls": ["turn:10.132.0.71:3478"]
 }]
 
+MAX_MESSAGE_SIZE = 1000 * 16  # 16KB
+
 
 class FileTracker:
 
@@ -35,37 +37,44 @@ class FileTracker:
         async with aiohttp.ClientSession() as session:
             await session.post(f"{SIGNALING_SERVER}/register",
                                json={"clientId": task_id})
-            self.pc = None
+
             while self.running:
-                logging.info("Listening for messages")
+                logging.info("Listening for connections")
                 async with session.get(
                         f"{SIGNALING_SERVER}/message?clientId={task_id}"
                 ) as resp:
                     if resp.status == 200:
                         data = await resp.json()
                         if data['type'] == 'offer':
-                            self.pc = await create_peer_connection()
-                            await self.pc.setRemoteDescription(
+                            pc = await create_peer_connection(task_id)
+                            await pc.setRemoteDescription(
                                 RTCSessionDescription(sdp=data['sdp'],
                                                       type=data['type']))
-                            answer = await self.pc.createAnswer()
-                            await self.pc.setLocalDescription(answer)
+                            answer = await pc.createAnswer()
+                            await pc.setLocalDescription(answer)
                             await session.post(
                                 f"{SIGNALING_SERVER}/offer",
                                 json={
                                     "receiverId": data['senderId'],
                                     "type": "answer",
-                                    "sdp": self.pc.localDescription.sdp
+                                    "sdp": pc.localDescription.sdp
                                 })
-            await self.pc.close()
             logging.info("Stopped listening for messages")
 
     async def close(self):
         self.running = False
 
 
-async def get_directory_contents(task=""):
-    return os.listdir('./' + task)
+async def get_directory_contents(path):
+    contents = []
+    dir = os.listdir(path)
+    for file in dir:
+        if os.path.isdir(path + file + "/"):
+            contents.append(
+                {file: await get_directory_contents(path + file + "/")})
+        else:
+            contents.append(file)
+    return contents
 
 
 async def read_file(filename):
@@ -93,34 +102,25 @@ async def tail_file(filename, lines=10):
         return b'\n'.join(content.split(b'\n')[-lines:])
 
 
-async def create_peer_connection():
+async def create_peer_connection(task_id):
     pc = RTCPeerConnection()
     pc.configuration = {"iceServers": ICE_SERVERS}
+    path = task_id + "/output/artifacts/"
 
     @pc.on("datachannel")
     def on_datachannel(channel):
 
         @channel.on("message")
         async def on_message(message):
-            if message == "list":
-                contents = await get_directory_contents()
-                channel.send(json.dumps({"type": "list", "data": contents}))
+            if message == "ls":
+                contents = await get_directory_contents(path)
+                channel.send(json.dumps(contents))
 
-            elif message == "pwd":
-                logging.info("GOT pwd")
-                channel.send(json.dumps({"type": "pwd", "data": os.getcwd()}))
-
-            elif message.startswith(("download:", "cat:")):
+            elif message.startswith(("download:")):
                 filename = message.split(":")[1]
-                content = await read_file(filename)
+                content = await read_file(path + filename)
                 logging.info("Sending file %s to peer", filename)
-                header = json.dumps({
-                    "type": message.split(":")[0],
-                    "filename": filename,
-                }).encode()
-                message = header + b'\n' + content[:1024]
-                logging.info("Header: %s", header)
-                logging.info("Content: %s", content[:1024])
+                message = content[-1024:]
                 channel.send(message)
 
             elif message.startswith("tail:"):
@@ -128,10 +128,9 @@ async def create_peer_connection():
                 content = await tail_file(filename)
                 channel.send(content)
 
-            elif message.startswith("cd:"):
-                path = message.split(":")[1]
-                os.chdir(path)
-                contents = await get_directory_contents()
-                channel.send(json.dumps({"type": "cd", "data": contents}))
+        @channel.on("close")
+        async def on_close():
+            await pc.close()
+            logging.info("PeerConnection closed")
 
     return pc
