0851d00e34c3029501cab411787d30a16b477eb3
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-11-12 10:34:52 +0000

    Update executer.py

--- task-runner/task_runner/executers/swan/executer.py
+++ task-runner/task_runner/executers/swan/executer.py
@@ -8,10 +8,20 @@ from task_runner import executers
 class SWANExecuter(executers.BaseExecuter):
     """Executer class for the SWAN simulator."""
 
+    def _create_command(self,binary, input_file, np, is_mpi):
+        """Create the command to run the SWAN simulator.
+        Args:
+            binary (str): The binary to run.
+            input_file (str): The input file to run.
+            np (int): The number of CPUs to use.
+        """
+        if input_file.endswith(".swn"):
+            input_file = input_file[:-4]
+        return executers.Command(f"{binary} -input {input_file} -mpi {np}", is_mpi)
     def execute(self):
         sim_binary = self.args.command
         input_filename = self.args.input_filename
-        np = self.args.n_vcpus
+        np = self.args.n_vcpus or 1
 
         input_dir = os.path.join(self.working_dir, self.args.sim_dir)
         shutil.copytree(input_dir, self.artifacts_dir, dirs_exist_ok=True)
@@ -19,15 +29,8 @@ class SWANExecuter(executers.BaseExecuter):
         # Swanrun uses internal MPI
         # we call apptainer run ... swanrun ... -mpi np
         if sim_binary == "swanrun":
-            # if no np is provided, default to 1
-            np = np or 1
-            if input_filename.endswith(".swn"):
-                #remove the .swn extension
-                cmd_str = f"{sim_binary} -input {input_filename[:-4]} -mpi {np}"
-            else:
-                cmd_str = f"{sim_binary} -input {input_filename} -mpi {np}"
-            
-            cmd = executers.Command(cmd_str, is_mpi=False)
+            cmd = self._create_command(sim_binary, input_filename, np, is_mpi=False)
+
         # we call mpirun ... apptainer ... Swan.exe
         # works with clusters
         elif sim_binary == "swan.exe":
@@ -36,9 +39,8 @@ class SWANExecuter(executers.BaseExecuter):
                 self.mpi_config.extra_args.extend(["-np", f"{self.args.n_vcpus}"])
             if self.args.use_hwthread:
                 self.mpi_config.extra_args.extend(["--use-hwthread-cpus"])
-
-            cmd_str = f"{sim_binary}"
-            cmd = executers.Command(cmd_str, is_mpi=True)
+            
+            cmd = executers.Command(sim_binary, is_mpi=True)
         else:
             raise ValueError("Invalid sim_binary")
 
