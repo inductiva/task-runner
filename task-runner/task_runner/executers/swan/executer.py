0feb743e6c7b785ee67ccbbc75ae0b5ee2dc6670
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-11-12 10:14:02 +0000

    swanrun now only runs on one machine and swan.exe works with clusters

--- task-runner/task_runner/executers/swan/executer.py
+++ task-runner/task_runner/executers/swan/executer.py
@@ -11,27 +11,36 @@ class SWANExecuter(executers.BaseExecuter):
     def execute(self):
         sim_binary = self.args.command
         input_filename = self.args.input_filename
+        np = self.args.n_vcpus
 
         input_dir = os.path.join(self.working_dir, self.args.sim_dir)
         shutil.copytree(input_dir, self.artifacts_dir, dirs_exist_ok=True)
 
-        if self.args.n_vcpus:
-            self.mpi_config.extra_args.extend(["-np", f"{self.args.n_vcpus}"])
-
-        if self.args.use_hwthread:
-            self.mpi_config.extra_args.extend(["--use-hwthread-cpus"])
-
+        # Swanrun uses internal MPI
+        # we call apptainer run ... swanrun ... -mpi np
         if sim_binary == "swanrun":
+            # if no np is provided, default to 1
+            np = np or 1
             if input_filename.endswith(".swn"):
                 #remove the .swn extension
-                cmd_str = f"{sim_binary} -input {input_filename[:-4]}"
+                cmd_str = f"{sim_binary} -input {input_filename[:-4]} -mpi {np}"
             else:
-                cmd_str = f"{sim_binary} -input {input_filename}"
+                cmd_str = f"{sim_binary} -input {input_filename} -mpi {np}"
+            
+            cmd = executers.Command(cmd_str, is_mpi=False)
+        # we call mpirun ... apptainer ... Swan.exe
+        # works with clusters
         elif sim_binary == "swan.exe":
+            
+            if self.args.n_vcpus:
+                self.mpi_config.extra_args.extend(["-np", f"{self.args.n_vcpus}"])
+            if self.args.use_hwthread:
+                self.mpi_config.extra_args.extend(["--use-hwthread-cpus"])
+
             cmd_str = f"{sim_binary}"
+            cmd = executers.Command(cmd_str, is_mpi=True)
         else:
             raise ValueError("Invalid sim_binary")
 
-        cmd = executers.Command(cmd_str, is_mpi=True)
 
         self.run_subprocess(cmd, self.artifacts_dir)
