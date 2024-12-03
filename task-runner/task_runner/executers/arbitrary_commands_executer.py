f0b791a8aeb58a1a0e2707a44f9f7c9c0051f93d
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-12-03 16:19:33 +0000

    gets the working_dir from the client if not provided uses artifacts_dir

--- task-runner/task_runner/executers/arbitrary_commands_executer.py
+++ task-runner/task_runner/executers/arbitrary_commands_executer.py
@@ -12,9 +12,11 @@ class ArbitraryCommandsExecuter(executers.BaseExecuter):
     def execute(self):
         input_dir = os.path.join(self.working_dir, self.args.sim_dir)
 
+        working_dir = self.args.working_dir or self.artifacts_dir
+
         # Copy the input files to the artifacts directory
         shutil.copytree(input_dir, self.artifacts_dir, dirs_exist_ok=True)
 
         for command in self.args.commands:
             cmd = executers.Command.from_dict(command)
-            self.run_subprocess(cmd, self.artifacts_dir)
+            self.run_subprocess(cmd, working_dir=working_dir)
