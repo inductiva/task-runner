117c124710132ab4b4065f12463a14e77780b2de
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-12-03 16:28:49 +0000

    Update arbitrary_commands_executer.py

--- task-runner/task_runner/executers/arbitrary_commands_executer.py
+++ task-runner/task_runner/executers/arbitrary_commands_executer.py
@@ -12,11 +12,15 @@ class ArbitraryCommandsExecuter(executers.BaseExecuter):
     def execute(self):
         input_dir = os.path.join(self.working_dir, self.args.sim_dir)
 
-        working_dir = self.args.working_dir or self.artifacts_dir
+        run_subprocess_dir = self.artifacts_dir
+
+        if self.args.run_subprocess_dir:
+            run_subprocess_dir = os.path.join(self.artifacts_dir,
+                                              self.args.run_subprocess_dir)
 
         # Copy the input files to the artifacts directory
         shutil.copytree(input_dir, self.artifacts_dir, dirs_exist_ok=True)
 
         for command in self.args.commands:
             cmd = executers.Command.from_dict(command)
-            self.run_subprocess(cmd, working_dir=working_dir)
+            self.run_subprocess(cmd, working_dir=run_subprocess_dir)
