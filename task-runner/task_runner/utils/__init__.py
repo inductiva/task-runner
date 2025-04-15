1de2e62b1d2726386bc01049cae38b0d2174dbae
Henrique Preto <hpreto@inductiva.ai>
2025-04-15 14:26:16 +0100

    Delete unused global constant

--- task-runner/task_runner/utils/__init__.py
+++ task-runner/task_runner/utils/__init__.py
@@ -11,9 +11,6 @@ import datetime
 import time
 from functools import wraps
 
-from task_runner.utils.retries import retry
-
-INPUT_JSON_FILENAME = "input.json"
 INPUT_ZIP_FILENAME = "input.zip"
 OUTPUT_ZIP_FILENAME = "output.zip"
 OUTPUT_DIR = "output"
