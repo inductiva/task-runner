536ee64fd42e5af24bf2b611c8495c42338da6d3
Henrique Preto <hpreto@inductiva.ai>
2025-05-12 16:29:19 +0100

    Adjust exponential backoff

--- task-runner/task_runner/file_manager.py
+++ task-runner/task_runner/file_manager.py
@@ -123,7 +123,7 @@ class WebApiFileManager(BaseFileManager):
             event_logger: task_runner.BaseEventLogger) -> requests.Response:
         for attempt in tenacity.Retrying(
                 stop=tenacity.stop_after_attempt(5),
-                wait=tenacity.wait_exponential(),
+                wait=tenacity.wait_exponential(multiplier=10),
                 before_sleep=WebApiFileManager.make_fail_upload_hook(
                     task_id=task_id,
                     task_runner_uuid=task_runner_uuid,
