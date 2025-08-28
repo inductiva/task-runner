88703344a7dce92bdc63ef75eda599ba4ddf2f84
Bruno Grego <bgrego@inductiva.ai>
2025-08-28 15:52:51 +0100

    Add issuer to TaskRunnerTerminated event

--- task-runner/task_runner/events/schemas/task_runner.py
+++ task-runner/task_runner/events/schemas/task_runner.py
@@ -9,11 +9,12 @@ from task_runner import task_status
 # Shared properties of task-runner events.
 class TaskRunnerEvent(event_schemas.Event):
     uuid: uuid.UUID
+    issuer: Optional[str] = None
 
 
 # Task-runner down event.
 class TaskRunnerTerminated(TaskRunnerEvent):
     reason: task_status.TaskRunnerTerminationReason
-    detail: Optional[str]
+    detail: Optional[str] = None
     traceback: Optional[str] = None
     stopped_tasks: list[str]
