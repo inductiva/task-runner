4f1f1c297f2bc82b30bb7c897fae594c02c19ea9
rvalerio <rvalerio@inductiva.ai>
2025-10-03 11:47:33 +0100

    Improve max idle time

--- task-runner/task_runner/machine_group.py
+++ task-runner/task_runner/machine_group.py
@@ -7,15 +7,10 @@ from task_runner.utils import config
 
 class MachineGroupInfo:
 
-    def __init__(self,
-                 id: uuid.UUID,
-                 name: str,
-                 local_mode: bool,
-                 max_idle_time: int = None):
+    def __init__(self, id: uuid.UUID, name: str, local_mode: bool):
         self.id = id
         self.name = name
         self.local_mode = local_mode
-        self.max_idle_time = max_idle_time
 
     @classmethod
     def from_api(cls, api_client: ApiClient):
@@ -30,14 +25,12 @@ class MachineGroupInfo:
             return machine_group_info
 
         if machine_group_info.name and (
-                machine_group_data :=
-                api_client.get_started_machine_group_by_name(
+                machine_group_id :=
+                api_client.get_started_machine_group_id_by_name(
                     machine_group_info.name)):
             logging.info("Specified machine group exists: %s",
                          machine_group_info.name)
-            machine_group_info.id = machine_group_data["id"]
-            machine_group_info.max_idle_time = machine_group_data[
-                "max_idle_time"]
+            machine_group_info.id = machine_group_id
             return machine_group_info
 
         if not machine_group_info.local_mode:
