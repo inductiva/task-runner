57e453bf46eb714bc3686580e596aa120ef13300
rcvalerio <rvalerio@inductiva.ai>
2025-12-12 10:08:53 +0000

    Add exit code to stdout and stderr

--- task-runner/task_runner/executers/base_executer.py
+++ task-runner/task_runner/executers/base_executer.py
@@ -291,6 +291,12 @@ class BaseExecuter(ABC):
             exit_code = self.subprocess.wait()
             execution_time = time.perf_counter() - start
 
+            exit_code_message = f"# EXIT CODE: {exit_code}\n\n"
+            stdout.write(exit_code_message)
+            stderr.write(exit_code_message)
+            stdout.flush()
+            stderr.flush()
+
             stdout.write("\n -------\n")
             stderr.write("\n -------\n")
 
